327f3d1c3e4f94502f496ccc6737e68dd3c2f6a9
Max Hart <maxhart5000@icloud.com>
2023-10-21 12:05:56 +0800

    Added comments to both Main.java and AdventureGame.java. Some tidying up and cleaning in the code.

--- src/main/java/dev/hartcode/Main.java
+++ src/main/java/dev/hartcode/Main.java
@@ -6,20 +6,30 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        // Create an instance of the AdventureGame class.
         AdventureGame adventureGame = new AdventureGame();
+
+        // Start the game at the specified location ("Road").
         adventureGame.play("Road");
 
+        // Create a Scanner object for user input.
         Scanner scanner = new Scanner(System.in);
 
-        while(true){
-            String direction = scanner.nextLine().trim().toUpperCase().substring(0,1);
-            if(direction.equals("Q")){
+        while (true) {
+            // Read user input, trim leading/trailing whitespace, and convert to uppercase.
+            String direction = scanner.nextLine().trim().toUpperCase().substring(0, 1);
+
+            // Check if the user wants to quit the game.
+            if (direction.equals("Q")) {
                 System.out.println("The game is shutting down...");
-                break;
-            }else{
+                break; // Exit the game loop.
+            } else {
+                // Move the player in the specified direction.
                 adventureGame.move(direction);
             }
         }
 
+        // Close the Scanner and exit the program.
+        scanner.close();
     }
 }
--- src/main/java/dev/hartcode/game/AdventureGame.java
+++ src/main/java/dev/hartcode/game/AdventureGame.java
@@ -5,6 +5,8 @@ import java.util.HashMap;
 import java.util.List;
 
 public class AdventureGame {
+
+    // Define the game locations and connections.
     private static final String GAME_LOCATIONS = """
     Forest, A wild and densely packed forest surrounds you, E: Mountain, S: Hill
     Mountain, A vast and ominous mountain towers before you, W: Forest, E: Lake, S: Road
@@ -16,76 +18,96 @@ public class AdventureGame {
     Valley, A beautiful valley stretches all around you dazzling you, N: Road, E: Stream, W: Cave
     Stream, A gentle and zen stream trickles over your toes, N: Well house, W: Valley
     """;
-    private enum Compass{
-        N,E,S,W;
-        private static final String[] s = {"North", "East", "South", "West"};
-        public String getString(){
+
+    // Enum to represent compass directions (N, E, S, W).
+    private enum Compass {
+        N, E, S, W;
+
+        private static final String[] s = { "North", "East", "South", "West" };
+
+        public String getString() {
             return s[this.ordinal()];
         }
     }
-    private record Location(String description, HashMap<Compass, String> nextPlaces ){}
+
+    // Record to represent a location with its description and connections to other locations.
+    private record Location(String description, HashMap<Compass, String> nextPlaces) {}
+
+    // Variable to keep track of the last visited location.
     private String lastPlace;
+
+    // Hash map to store locations and their details.
     private final HashMap<String, Location> adventureMap = new HashMap<>();
 
-    public AdventureGame(){
+    // Default constructor to load the game with predefined locations.
+    public AdventureGame() {
         this(null);
     }
-    public AdventureGame(String customLocation){
+
+    // Constructor to load the game with predefined locations and custom locations if provided.
+    public AdventureGame(String customLocation) {
         loadLocations(GAME_LOCATIONS);
-        if(customLocation!=null){
+        if (customLocation != null) {
             loadLocations(customLocation);
         }
     }
-    private void loadLocations(String locationData){
-        for(String s : locationData.split("\\R")){
-            String[] parts = s.split(",",3);
+
+    // Method to load locations and their connections from the provided data.
+    private void loadLocations(String locationData) {
+        for (String s : locationData.split("\\R")) {
+            String[] parts = s.split(",", 3);
             Arrays.asList(parts).replaceAll(String::trim);
-            HashMap<Compass,String> nextPlaces = loadNextPlaces(parts[2]);
+            HashMap<Compass, String> nextPlaces = loadNextPlaces(parts[2]);
             Location location = new Location(parts[1], nextPlaces);
             adventureMap.put(parts[0], location);
         }
-        // adventureMap.forEach((k,v) -> System.out.println("%s: %s".formatted(k,v)));
-
     }
-    private HashMap<Compass, String> loadNextPlaces(String parts){
-        HashMap<Compass,String> nextPlaces = new HashMap<>();
+
+    // Method to load connections to other locations for a given location.
+    private HashMap<Compass, String> loadNextPlaces(String parts) {
+        HashMap<Compass, String> nextPlaces = new HashMap<>();
         List<String> nextSteps = Arrays.asList(parts.split(","));
         nextSteps.replaceAll(String::trim);
-        for(String string : nextSteps){
+        for (String string : nextSteps) {
             String[] s = string.split(":");
             Compass compass = Compass.valueOf(s[0].trim());
             String destination = s[1].trim();
-            nextPlaces.put(compass,destination);
+            nextPlaces.put(compass, destination);
         }
         return nextPlaces;
     }
-    private void visit(Location location){
+
+    // Method to visit and display information about the current location.
+    private void visit(Location location) {
         System.out.println(location.description);
         System.out.println("From here, you can see: ");
-        location.nextPlaces.forEach((k,v) -> System.out.printf(
-                "\t* A %s lies to the %s (%s)%n", v,k.getString(),k));
+        location.nextPlaces.forEach((k, v) -> System.out.printf(
+                "\t* A %s lies to the %s (%s)%n", v, k.getString(), k));
         System.out.println("Select your compass direction (Q - quit) >> ");
     }
-    public void move(String direction){
+
+    // Method to move to the next location based on the specified direction.
+    public void move(String direction) {
         var nextPlaces = adventureMap.get(lastPlace).nextPlaces;
         String nextPlace;
-        if("NESW".contains(direction)){
+        if ("NESW".contains(direction)) {
             nextPlace = nextPlaces.get(Compass.valueOf(direction));
-            if(nextPlace!=null){
+            if (nextPlace != null) {
                 play(nextPlace);
             }
-        }else{
+        } else {
             System.out.println("Invalid direction!");
         }
     }
-    public void play(String location){
-        if(adventureMap.containsKey(location)){
+
+    // Method to play the game by moving to the specified location.
+    public void play(String location) {
+        if (adventureMap.containsKey(location)) {
             Location nextLocation = adventureMap.get(location);
             lastPlace = location;
             visit(nextLocation);
-        }else{
-            System.out.println(location+" is an invalid location");
+        } else {
+            System.out.println(location + " is an invalid location");
         }
     }
-
 }
